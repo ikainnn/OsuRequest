dbd5f0a1e2865f18cef45a2b1e6869805f37a1da
nyako <nyakonyns@gmail.com>
2022-04-10 00:16:15 -0300

    ✨

--- Source/Api/Beatconnect.cpp
+++ Source/Api/Beatconnect.cpp
@@ -2,6 +2,7 @@
 
 #include "fmt/format.h"
 
+#include "../../Include/Logging.hpp"
 #include "../../Include/KaedeRequest/Net/Client.hpp"
 
 namespace kaede::api
@@ -13,6 +14,11 @@ namespace kaede::api
 
     auto download_beatmap(const std::filesystem::path path, const Beatmap& beatmap) -> void
     {
+        if (!std::filesystem::exists(path))
+        {
+            KAEDE_ERRO(fmt::format("Couldn't find path {}", path.string())); return;
+        }
+
         const auto filename = fmt::format("{}/{} {} - {}.osz", path.string(), beatmap.beatmapsetID, beatmap.songInfo.artist, beatmap.songInfo.title);
 
         std::ofstream beatmapStream { filename, std::ios::binary };
