fe6a96c461a41f8af8b6e07f75eda15d61a0903a
nyako <nyakonyns@gmail.com>
2022-05-07 22:47:56 -0300

    ;v

--- Include/Logging.hpp
+++ Include/Logging.hpp
@@ -5,8 +5,8 @@
 
 #define BASIC_LOG(tag, msg) std::cout << tag << msg << '\n';
 
-#define KAEDE_WARN(msg) BASIC_LOG("[WARN]", msg)
-#define KAEDE_ERRO(msg) BASIC_LOG("[ERRO]", msg)
-#define KAEDE_INFO(msg) BASIC_LOG("[INFO]", msg)
+#define KAEDE_WARN(msg) BASIC_LOG("[WARN] ", msg)
+#define KAEDE_ERRO(msg) BASIC_LOG("[ERRO] ", msg)
+#define KAEDE_INFO(msg) BASIC_LOG("[INFO] ", msg)
 
 #endif
