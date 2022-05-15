c0484999d7837571321457b33bb40e84e3fb39a7
nyako <nyakonyns@gmail.com>
2022-05-14 21:06:16 -0300

    .

--- Include/Logging.hpp
+++ Include/Logging.hpp
@@ -4,19 +4,16 @@
 #include <iostream>
 #include <format>
 
-#ifndef KAEDE_MACROS_IMPL
-#define KAEDE_MACROS_IMPL
-    #ifdef KAEDE_DEBUG
-        #define BASIC_LOG(tag, msg) std::cout << std::format("{} {} \n", tag, msg)
+#ifdef KAEDE_DEBUG
+    #define BASIC_LOG(tag, msg) std::cout << std::format("{} {} \n", tag, msg)
 
-        #define KAEDE_WARN(msg) BASIC_LOG("[WARN]", msg)
-        #define KAEDE_ERRO(msg) BASIC_LOG("[ERRO]", msg)
-        #define KAEDE_INFO(msg) BASIC_LOG("[INFO]", msg)
-    #else
-        #define KAEDE_WARN(msg)
-        #define KAEDE_ERRO(msg)
-        #define KAEDE_INFO(msg)
-    #endif
+    #define KAEDE_WARN(msg) BASIC_LOG("[WARN]", msg)
+    #define KAEDE_ERRO(msg) BASIC_LOG("[ERRO]", msg)
+    #define KAEDE_INFO(msg) BASIC_LOG("[INFO]", msg)
+#else
+    #define KAEDE_WARN(msg)
+    #define KAEDE_ERRO(msg)
+    #define KAEDE_INFO(msg)
 #endif
 
 #endif
