b9e650e2a2cbbd7f30861e1ada5c428adf4f45ff
nyako <nyakonyns@gmail.com>
2022-05-22 13:50:12 -0400

    detect debug

--- Include/Logging.hpp
+++ Include/Logging.hpp
@@ -14,7 +14,7 @@ namespace osu_request::logging
 {
     void print(std::string_view _tag, std::string_view _format, auto&&... _args)
     {
-    #if OSU_MAN_DEBUG_BUILD
+    #if DEBUG_BUILD
         const auto clock       = std::chrono::system_clock::now();
         const auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(clock.time_since_epoch()).count();
         fmt::print("{} {} {}", _tag, currentTime, fmt::format(fmt::runtime(_format), _args...));
